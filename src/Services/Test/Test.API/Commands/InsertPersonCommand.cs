e601bed86011712a570a11062a37dd2e0f83108f
teli-siddu <teli.siddu.n@gmail.com>
2024-06-14 00:43:44 +0530

    changes moved

---
+++ src/Services/Test/Test.API/Commands/InsertPersonCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using Test.API.Models;
+
+namespace Test.API.Commands
+{
+    public record InsertPersonCommand(string FirstName, string LastName):IRequest<PersonModel>;
+   
+}
---
+++ src/Services/Test/Test.API/DataAccess/DataAccess.cs
@@ -0,0 +1,29 @@
+﻿using Test.API.Models;
+
+namespace Test.API.DataAccess
+{
+    public class DataAccess : IDataAccess
+    {
+        List<PersonModel> people = new();
+        public DataAccess()
+        {
+            people.Add(new PersonModel { Id = 1, FirstName = "siddu", LastName = "teli" });
+            people.Add(new PersonModel { Id = 2, FirstName = "Rakesh", LastName = "K" });
+        }
+        public List<PersonModel> GetPeople()
+        {
+            return people;
+        }
+        public PersonModel GetPersonById(int id)
+        {
+            return people.FirstOrDefault(x=>x.Id==id);
+        }
+        public PersonModel InsertPerson(string firstName, string lastName)
+        {
+            PersonModel person = new() { FirstName = firstName, LastName = lastName };
+            person.Id = people.Max(x => x.Id) + 1;
+            people.Add(person);
+            return person;
+        }
+    }
+}
